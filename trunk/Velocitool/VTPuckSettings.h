710f7bbdee3a9030a15766400dba24f067397f8e
eric.noyau <eric.noyau@a5aadc7c-ec76-11dd-a738-5fe2e3cfbcc5>
2009-03-09 01:59:21 +0000

    Added a fake device to speed up implementation. Got the preference panel to work. Still need to connect the panel to something.

---
+++ trunk/Velocitool/VTPuckSettings.h
@@ -0,0 +1,25 @@
+
+#import <Cocoa/Cocoa.h>
+
+@interface VTPuckSettings : NSObject {
+    NSMutableDictionary *_settings;
+}
+
+@property (retain) id recordRate;
+@property (retain) id declination;
+@property (retain) id speedUnitOfMeasurement;
+@property (retain) id speedDamping;
+@property (retain) id headingDamping;
+@property (retain) id maxSpeedMode;
+@property (retain) id barGraphEnabled;
+@property (retain) id deviceOperationOption;
+
+@property (readonly) NSString *barGraphLabel;
+@property (readonly) BOOL barGraphState;
+@property (readonly) BOOL compassEnabled;
+@property (readonly) BOOL everythingOff;
+
++ (VTPuckSettings *)settingsWithDictionary:(NSDictionary *)settings;
+- (NSDictionary *)settingsDictionary;
+@end
+
