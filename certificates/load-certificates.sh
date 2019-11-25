d9740e6629a7c4f46a05cf5a0b3baf57e5d159e9
Thomas Sarlandie <thomas@sarlandie.net>
2019-11-25 18:39:27 +0100

    ci: load certificates

---
+++ certificates/load-certificates.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# Load certificates so that codesign can sign the app and the dmg later.
+# The certificates.p12 file must be exported with a password and the key
+# need to be provided by an environment variable CERT_PASSWORD
+
+FOLDER=$(pwd)
+KEYCHAIN_PASSWORD=some_unsecure_password
+KEY_CHAIN=macos-build.keychain
+
+security create-keychain -p $KEYCHAIN_PASSWORD $KEY_CHAIN
+# Make the keychain the default so identities are found
+security default-keychain -s $KEY_CHAIN
+# Unlock the keychain
+security unlock-keychain -p $KEYCHAIN_PASSWORD $KEY_CHAIN
+# Set keychain locking timeout to 3600 seconds
+security set-keychain-settings -t 3600 -u $KEY_CHAIN
+
+# Add certificates to keychain and allow codesign to access them
+security import $FOLDER/certificates/developer_id.cer -k $KEY_CHAIN -T /usr/bin/codesign
+security import $FOLDER/certificates/certificates.p12 -k $KEY_CHAIN -P $CERT_PASSWORD -T /usr/bin/codesign
+
+echo "Add keychain to keychain-list"
+security list-keychains -s $KEY_CHAIN
+
