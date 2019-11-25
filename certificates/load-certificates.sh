883f1d3bb5be57a0e06d75524498d92e437a39c5
Thomas Sarlandie <thomas@sarlandie.net>
2019-11-25 18:45:49 +0100

    fix certificate path

--- certificates/load-certificates.sh
+++ certificates/load-certificates.sh
@@ -4,7 +4,6 @@
 # The certificates.p12 file must be exported with a password and the key
 # need to be provided by an environment variable CERT_PASSWORD
 
-FOLDER=$(pwd)
 KEYCHAIN_PASSWORD=some_unsecure_password
 KEY_CHAIN=macos-build.keychain
 
@@ -17,8 +16,8 @@ security unlock-keychain -p $KEYCHAIN_PASSWORD $KEY_CHAIN
 security set-keychain-settings -t 3600 -u $KEY_CHAIN
 
 # Add certificates to keychain and allow codesign to access them
-security import $FOLDER/certificates/developer_id.cer -k $KEY_CHAIN -T /usr/bin/codesign
-security import $FOLDER/certificates/certificates.p12 -k $KEY_CHAIN -P $CERT_PASSWORD -T /usr/bin/codesign
+security import developer_id.cer -k $KEY_CHAIN -T /usr/bin/codesign
+security import certificates.p12 -k $KEY_CHAIN -P $CERT_PASSWORD -T /usr/bin/codesign
 
 echo "Add keychain to keychain-list"
 security list-keychains -s $KEY_CHAIN
